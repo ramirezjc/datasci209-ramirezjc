9e2224e5fa30eff7df412c04e1660d2bcc5b2e9c
ramirezjc <jeremy.ramirez@berkeley.edu>
2024-08-04 16:40:19 -0700

    Add team members to the Our Team section

--- script.js
+++ script.js
@@ -1,26 +1,2 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const sections = document.querySelectorAll('section');
-    const navLinks = document.querySelectorAll('nav ul li a');
-
-    function updateActiveLink() {
-        let currentSection = '';
-
-        sections.forEach(section => {
-            const sectionTop = section.offsetTop - 80; // Adjust this value based on your fixed header height
-            const sectionHeight = section.clientHeight;
-            if (window.pageYOffset >= sectionTop && window.pageYOffset < sectionTop + sectionHeight) {
-                currentSection = section.getAttribute('id');
-            }
-        });
-
-        navLinks.forEach(link => {
-            link.classList.remove('active');
-            if (link.getAttribute('href').includes(currentSection)) {
-                link.classList.add('active');
-            }
-        });
-    }
-
-    window.addEventListener('scroll', updateActiveLink);
-    updateActiveLink();  // Initialize active link on page load
 });
